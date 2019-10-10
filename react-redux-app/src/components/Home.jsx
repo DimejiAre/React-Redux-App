e13cce9e7673c78fb548c7cb55146b410363a139
DimejiAre <olardi2@gmail.com>
2019-10-10 14:43:22 +0100

    create Home component

---
+++ react-redux-app/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/Home.scss';
+
+function Home(){
+    return (
+        <div class='home'>
+        <h1>Welcome to Beer recipes page</h1>
+        <img src={'https://image.freepik.com/free-photo/beer-bottles-frame-wheat-with-copy-space_23-2148306003.jpg'} alt={'beer homepage'}/>
+    </div>
+    )
+}
+
+export default Home;
