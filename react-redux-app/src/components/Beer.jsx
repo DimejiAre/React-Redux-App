31b14d3f9116687c1ce064dae99b8504f9a8fe1e
DimejiAre <olardi2@gmail.com>
2019-10-10 14:16:02 +0100

    add basic styling to components

--- react-redux-app/src/components/Beer.jsx
+++ react-redux-app/src/components/Beer.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as actionCreators from '../state/actionCreators';
+import '../scss/Beers.scss';
 
 function Beer({beer}){
     return (
-        <div>
-            <h2>{'Name: ' + beer.name}</h2>
-            <p>{'Description: ' + beer.description}</p>
+        <div className='beer'>
+            <h2>{beer.name}</h2>
+            <img src={beer.image_url} alt={beer.name} />
+            <p>{beer.tagline}</p>
+            <div class='beer-info'>
+                <p><strong>Description: </strong>{beer.description}</p>
+                <p><strong>First Brewed: </strong>{beer.first_brewed}</p>
+                <span><strong>Food Pairing: </strong></span>
+                <ul>{beer.food_pairing? beer.food_pairing.map(food => (
+                    <li>{food}</li>
+                )): null}</ul>
+                <p><strong>Brewers Tips: </strong>{beer.brewers_tips}</p>
+            </div>
         </div>
     )
 }
--- react-redux-app/src/components/Beers.jsx
+++ react-redux-app/src/components/Beers.jsx
@@ -1,21 +1,23 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import * as actionCreators from '../state/actionCreators';
-import Beer from './Beer'
+import Beer from './Beer';
+import '../scss/Beers.scss';
 
 export function Beers(props){
-    debugger
     const {beers, getBeers} = props;
     useEffect(()=> {
         getBeers();
     },[])
 
     return (
-        <div>
+        <div className='beers'>
             {
+                beers?
                 beers.map(beer => (
                     <Beer key={beer.id} beer={beer} />
                 ))
+                : null
             }
         </div>
     )
