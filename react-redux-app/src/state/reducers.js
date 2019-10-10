bbef6e8d57f5ae358b0907a54ca397e091144680
DimejiAre <olardi2@gmail.com>
2019-10-10 13:40:49 +0100

    add functionality to get data from api and save to state

--- react-redux-app/src/state/reducers.js
+++ react-redux-app/src/state/reducers.js
@@ -4,6 +4,8 @@ const initialBeers = [{name: "something", id: 1, description: "something"}]
 
 export function beerReducer(state = initialBeers,action){
     switch(action.type){
+        case types.ADD_BEERS:
+            return action.payload
         default:
             return state
     }
