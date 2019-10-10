a6feab02b5c40d003669faecad611bdf60757d70
DimejiAre <olardi2@gmail.com>
2019-10-10 14:43:49 +0100

    create NavBar component

---
+++ react-redux-app/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/NavBar.scss'
+
+function NavBar(){
+    return(
+        <nav>
+            <h2>Brewdog Beer Recipes</h2>
+            <p>Beers</p>
+            <p>FindBeer</p>
+        </nav>
+    )
+}
+
+export default NavBar;
